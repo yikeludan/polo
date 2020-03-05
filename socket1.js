ec5da0ea060de19252069ea37b43783fc79ccdc4
yikeludan <820174300@qq.com>
2020-03-05 19:27:38 +0800

    'websocket模块修改'

--- socket1.js
+++ socket1.js
@@ -1,19 +1,19 @@
 /* 实例化外部依赖 */
 let Koa = require("koa");
 const Koa1 = require("koa");
-const app = new Koa1();
+const app1 = new Koa1();
 
 let WebSocket = require("koa-websocket");
 
 /* 实例化 WebSocket, 实例化储存所有上线文数组 并分配监听的端口 */
-let app1 = WebSocket(new Koa());
+let app = WebSocket(new Koa());
 let ctxs = [];
 let ctxs1 = {};
-app.listen(5000)
-app1.listen(3030);
+app1.listen(5000)
+app.listen(3030);
 
 /* 实现简单的接发消息 */
-app1.ws.use((ctx, next) => {
+app.ws.use((ctx, next) => {
     /* 每打开一个连接就往 上线文数组中 添加一个上下文 */
     const query = ctx.request.query
     ctxs.push(ctx);
@@ -22,7 +22,7 @@ app1.ws.use((ctx, next) => {
     ctx.websocket.on("message", (message) => {
 
         const aa = ctx.request.query
-         ctxs1[aa.toid].websocket.send(message);
+        ctxs1[aa.toid].websocket.send(message);
     });
     ctx.websocket.on("close", (message) => {
         /* 连接关闭时, 清理 上下文数组, 防止报错 */
