862251a106196982778998054ae0a97264e059a1
yikeludan <820174300@qq.com>
2020-03-05 18:08:49 +0800

    'websocket模块修改'

--- app.js
+++ app.js
@@ -53,6 +53,9 @@ wss.on('connection',function(ws,req){
     let i = req.url;//提取网址参数
     let u = i.match(/(?<=:).+?$/);              //提取发给谁
     let m = i.match(/(?<=\?)[^:]+?(?=:|$)/);    //提取我是谁,这部分代码只有第一次连接的时候运行,如果后面连接的m值相同,前面的连接会被覆盖身份
+
+    console.log(user)
+
     user.set(m,ws)
     //user[m] = ws
     console.log(user)
