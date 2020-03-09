f453ffcc3feef350e09e0c1717c3ad877edb2f48
yikeludan <820174300@qq.com>
2020-03-10 02:47:23 +0800

    '修改bug'

--- static/Chat.vue
+++ static/Chat.vue
@@ -7,15 +7,15 @@
     <div class = "msgBox">
       <div class = "msgBox1">
         <el-avatar :size="45">
-          <img src="https://sf3-ttcdn-tos.pstatp.com/img/lark.avatar/da66000e46346df6b51c~72x72.webp"/>
+          <img :src="applyUser.url"/>
         </el-avatar>
       </div>
       <div class = "msgBox2">
         <div class = "msgBox21">
-         一颗卤蛋想添加你为好友
+         {{applyUser.name}}想添加你为好友
         </div>
         <div class = "msgBox22">
-          <span class="sp1">  <el-button type="success" icon="el-icon-check" circle></el-button>
+          <span class="sp1">  <el-button type="success" @click="agreeApply" icon="el-icon-check" circle></el-button>
 </span><span class="sp2">  <el-button type="danger" icon="el-icon-close" circle></el-button>
 </span>
         </div>
@@ -28,13 +28,13 @@
         <div class = "searchText">
             <div class = "searchText_input"><el-input class="elin-inputZD1" v-model="input" placeholder="请输入内容"></el-input></div>
             <div class = "searchText_button">
-              <el-button type="success" class="upButton" icon="el-icon-plus" circle></el-button>
+              <el-button type="success" class="upButton" @click="applyFriend" icon="el-icon-plus" circle></el-button>
 
             </div>
 
         </div>
 
-        <div class = "chatUserList">
+        <div class = "chatUserList11">
           <div class="chatScroList">
 
 
@@ -100,9 +100,8 @@
               <div class="chatCenter">
                    <div class="chat_1">
                      <div class="chat_1_1">
-
-
-                       <div class="chatMsBox" v-for="(item,index) in ">
+                       <div class="chatMsBox" v-for="(item,index) in chatMsgList">
+                         {{item.msg}}
                        </div>
                      </div>
                    </div>
@@ -135,41 +134,23 @@
 
     data() {
       return {
-        myId:"1",
-        toId:"2",
+        myId:"",
+        toId:"",
+        input2:"",
         aa:[],
-        currentMyChatUser:{
-          name:"",
-          uid:"",
-          url:"",
-          myId:"",
-          toId:"",
-        },
-        currentChatUser:{
-          name:"",
-          uid:"",
-          url:"",
-          myId:"",
-          toId:"",
-        },
-        chatUserList:[{
-          url:"https://sf3-ttcdn-tos.pstatp.com/img/lark.avatar/da66000e46346df6b51c~72x72.webp",
-          name:"绘画盒子",
-          leftMsg:"121",
+        applyUser:{
+          name:"一颗卤蛋",
+          uid:"3333",
           msg:"撒旦",
-          uid:"",
+          msgId:"",
           date:"3-27 16:35",
-          id:'ffff'
+          id:'ffff',
+          leftMsg:"已成为好友",
+          url:"https://sf3-ttcdn-tos.pstatp.com/img/lark.avatar/da66000e46346df6b51c~72x72.webp",
         },
-          {
-            url:"https://sf3-ttcdn-tos.pstatp.com/img/lark.avatar/da66000e46346df6b51c~72x72.webp",
-            name:"绘画盒子1",
-            msg:"撒旦",
-            date:"3-27 16:35",
-            id:'ffff',
-            leftMsg:"121",
-            uid:"",
-          }],
+        currentChatUser:{},
+        chatMsgList:[],
+        chatUserList:[],
         isCollapse: true,
         textarea1:'',
         websock: null,
@@ -182,6 +163,8 @@
       };
     },
     created() {
+      const id = this.$route.query.id
+      this.myId = id
       this.initWebSocket();
     },
     destroyed() {
@@ -205,6 +188,24 @@
 
     },
     methods: {
+      getAllMyAndYouChatList(){
+        var that = this;
+        axios.get('http://127.0.0.1:5000/api/user/getAllMyAndYouChatList',{       // 还可以直接把参数拼接在url后边
+          params:{
+            uid:this.myId,
+            toid:this.toId
+          }
+        }).then(function(res){
+          if(res.data.length !=0){
+            console.log(res.data)
+            that.chatMsgList = [];
+            that.chatMsgBoxDom.scrollTop = that.chatMsgBoxDom.scrollHeight;
+          }
+
+        }).catch(function (error) {
+          console.log(error);
+        });
+      },
       getChatUserList(){
         var that = this;
         axios.get('http://127.0.0.1:5000/api/user/getChatUserList',{       // 还可以直接把参数拼接在url后边
@@ -213,7 +214,8 @@
           }
         }).then(function(res){
           if(res.data.length !=0){
-            that.chatUserList = JSON.parse(res.data)
+            that.chatUserList = res.data
+            console.log(that.chatUserList)
           }
 
         }).catch(function (error) {
@@ -221,44 +223,39 @@
         });
       },
       toggle(index){
-        this.currentChatUser.name = this.chatUserList[index].name;
-        this.currentChatUser.url = this.chatUserList[index].url;
-        this.currentChatUser.uid = this.chatUserList[index].uid;
+        this.currentChatUser = this.chatUserList[index];
+        this.toId = this.chatUserList[index].uid
+        this.getAllMyAndYouChatList()
         var childs = this.chatMsgBoxDom.childNodes;
         for(var i = childs.length - 1; i >= 0; i--) {
           this.chatMsgBoxDom.removeChild(childs[i]);
         }
-        let data = {
-          action:"goSubOneChat",//sendMsg  goSubOneChat
-          uid:this.myId,
-          toid:"",
-          msg:"初始化1",
-          date:""
-        }
-        //this.websocketsend(JSON.stringify(data));
       },
-      pushMsg(obj){
+      setMsgDIv(obj){
+        if(this.toId != obj.toId){
+          return;
+        }
         var div = document.createElement('div');
         var textNode  = document.createTextNode(obj.msg)
         div.className+=" chatMsBox";
-        if(this.textarea1.length<="43"&&this.textarea1.length>=35){
+        if(obj.msg.length<="43"&&obj.msg.length>=35){
           div.style.width = "600px"
           div.style.height = "30px"
         }
-        if(this.textarea1.length<="35"&&this.textarea1.length>=20){
+        if(obj.msg.length<="35"&&obj.msg.length>=20){
           div.style.width = "500px"
           div.style.height = "30px"
         }
-        if(this.textarea1.length<="20"&&this.textarea1.length>=10){
+        if(obj.msg.length<="20"&&obj.msg.length>=10){
           div.style.width = "400px"
           div.style.height = "30px"
         }
-        if(this.textarea1.length<="10"&&this.textarea1.length>="7"){
+        if(obj.msg.length<="10"&&obj.msg.length>="7"){
           div.style.width = "150px"
           div.style.height = "30px"
         }
 
-        if(this.textarea1.length<="7"){
+        if(obj.msg.length<="7"){
           div.style.width = "100px"
           div.style.height = "30px"
         }
@@ -266,6 +263,8 @@
         this.chatMsgBoxDom.appendChild(div)
         this.chatMsgBoxDom.scrollTop = this.chatMsgBoxDom.scrollHeight;
       },
+
+
       sendMs(value){
         var div = document.createElement('div');
         var textNode  = document.createTextNode(this.textarea1)
@@ -291,28 +290,66 @@
           div.style.width = "100px"
           div.style.height = "30px"
         }
-
         div.appendChild(textNode)
         this.chatMsgBoxDom.appendChild(div)
-        this.chatMsgBoxDom.scrollTop = this.chatMsgBoxDom.scrollHeight;
 
-        let data = {
-          action:"sendMsg",//sendMsg
-          uid:this.myId,
-          toid:"1",
-          msg:this.textarea1,
-          date:""
-        }
+        let obj = this.currentChatUser
+        obj.msg = this.textarea1;
+        obj.uid = this.myId;
+        obj.toId = this.toId;
+       // this.chatMsgList.push(obj)
+        this.chatMsgBoxDom.scrollTop = this.chatMsgBoxDom.scrollHeight;
         this.textarea1 =""
        // this.websocketsend(JSON.stringify(data));
 
         this.pushApplyDIv();
-        this.getFriends();
+        this.sendMsgAjax(obj);
       },
       pushApplyDIv(){
         console.log(this.msgBox)
         this.msgBox.className='msgBoxAn'
         this.myaudio.play();
+      },
+      agreeApply(){
+        this.agreeApplyAjax()
+        this.msgBox.className='msgBoxAn1'
+        this.setChatList2(this.applyUser)
+      },
+      agreeApplyAjax(){
+        var that = this;
+        axios.get('http://127.0.0.1:5000/api/user/agreeFriend',{       // 还可以直接把参数拼接在url后边
+          params:{
+            toId:this.applyUser.uid,
+            myId:this.myId
+          }
+        }).then(function(res){
+          if(res.data.length !=0){
+            console.log(res.data)
+            //that.chatUserList = res.data
+          }
+
+        }).catch(function (error) {
+          console.log(error);
+        });
+      },
+      pushApplyDivFast(obj){
+        this.applyUser = obj;
+        this.msgBox.className='msgBoxAn'
+        this.myaudio.play();
+      },
+      applyFriend(){
+        var that = this;
+        axios.get('http://127.0.0.1:5000/api/user/applyFriend',{       // 还可以直接把参数拼接在url后边
+          params:{
+            myId:this.myId,
+            name:this.input2
+          }
+        }).then(function(res){
+           console.log(res.data)
+
+        }).catch(function (error) {
+          console.log(error);
+        });
       },
       pushChatBox(){
         const chatList = document.querySelectorAll(".oneChatList")
@@ -356,8 +393,19 @@
         this.initWebSocket();
       },
       websocketonmessage(e){ //数据接收
+       // const redata = JSON.parse(e.data);
         const redata = JSON.parse(e.data);
-        console.log(e.data)
+        if(redata.action === 'agree'){
+          this.setChatList1(redata)
+        }
+        if(redata.action === 'apply'){
+          this.pushApplyDivFast(redata)
+        }
+        if(redata.action === 'msg'){
+          this.setMsgDIv(redata)
+          this.setChatList(redata)
+        }
+
       },
       websocketsend(Data){//数据发送
         this.websock.send(Data);
@@ -365,21 +413,52 @@
       websocketclose(e){  //关闭
         console.log('断开连接',e);
       },
-      setChatList(obj){
-        for(var i=0;i<=this.chatUserList.length;i++){
-          if(chatUserList[i].uid==obj.uid){
-             chatUserList.splice(i,1);
-             break;
+      setChatList1(obj){
+        let flag = true;
+
+        for(let i=0;i<this.chatUserList.length;i++){
+          if(this.chatUserList[i].uid === obj.uid){
+            this.chatUserList[i].leftMsg = obj.leftMsg;
+            flag = false;
+            break;
           }
         }
-        chatUserList.unshift(obj);
+        if(flag){
+          obj.leftMsg = "已同意请求"
+          this.chatUserList.unshift(obj);
+        }
       },
-      getFriends(){
-        var that = this;
-        axios.get('http://127.0.0.1:5000/api/user/myfriendMsg',{       // 还可以直接把参数拼接在url后边
-          params:{
-            title:'眼镜'
+      setChatList(obj){
+        let flag = true;
+        for(let i=0;i<this.chatUserList.length;i++){
+         if(this.chatUserList[i].uid === obj.toId){
+            this.chatUserList[i].leftMsg = obj.leftMsg;
+            flag = false;
+            break;
           }
+        }
+        if(flag){
+          this.chatUserList.unshift(obj);
+        }
+      },
+      setChatList2(obj){
+        let flag = true;
+        for(let i=0;i<this.chatUserList.length;i++){
+          if(this.chatUserList[i].uid === obj.toId){
+            this.chatUserList[i].leftMsg = obj.leftMsg;
+            flag = false;
+            break;
+          }
+        }
+        if(flag){
+          obj.leftMsg = "已经是好友了"
+          this.chatUserList.unshift(obj);
+        }
+      },
+      sendMsgAjax(obj){
+        var that = this;
+        axios.get('http://127.0.0.1:5000/api/user/sendMsg',{       // 还可以直接把参数拼接在url后边
+          params:obj
         }).then(function(res){
           that.aa = res.data
 
@@ -435,14 +514,14 @@
     margin-left:15px;
     color:black;
   }
-.chatUserList{
+.chatUserList11{
 
   width:257px;
   height:90vh;
   overflow: hidden;
 
 }
-  .chatUserList > div {
+  .chatUserList11 > div {
     /* 子容器比父容器的宽度多 17 px, 经测正好是滚动条的默认宽度 */
     width:267px;
 
@@ -634,6 +713,21 @@
      transition: all 0.5s;
      transition-timing-function:ease-out;
    }
+
+  .msgBoxAn1{
+    position:absolute;
+    width:300px;
+    height:100px;
+    z-index:5000;
+    left:75vw;
+    top:60vh;
+    border-radius: 10px;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+    transform: translateX(300px);
+    transition: all 0.5s;
+    transition-timing-function:ease-out;
+  }
+
   .msgBox{
     position:absolute;
     width:300px;
